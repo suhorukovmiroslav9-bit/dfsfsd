0452dc997471826cb78f1ae6001bde8086dc7103
suhorukovmiroslav9-bit <suhorukovmiroslav9@gmail.com>
2025-08-25 02:22:36 +0300

    Update injector.lua

--- injector.lua
+++ injector.lua
@@ -1,6 +1,6 @@
 local function LoadScript()
     local success, err = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ВАШ_НИК/xeno-script/main/xeno.lua", true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/suhorukovmiroslav9-bit/xeno-script/main/xeno.lua", true))()
     end)
     
     if not success then
